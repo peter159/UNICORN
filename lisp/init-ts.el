2e7ec8695fc6306bdee2fe619f33cf616454d465
peter159 <peter.linyi159@gmail.com>
2023-04-09 14:10:06 +0800

    add run ts file shorcut

--- lisp/init-ts.el
+++ lisp/init-ts.el
@@ -28,10 +28,25 @@
 
 (mark-time-here)
 
+(defun run-ts-file ()
+  "Compile and run the current TypeScript file."
+  (interactive)
+  (let* ((file-name (buffer-file-name))
+         (base-name (file-name-sans-extension file-name))
+         (js-file-name (concat base-name ".js")))
+    (if (not (file-exists-p js-file-name))
+        (progn
+          (message "Compiling %s to %s..." file-name js-file-name)
+          (call-process "tsc" nil nil nil file-name)))
+    (message "Running %s..." js-file-name)
+    (async-shell-command (concat "node " js-file-name))))
+
 (use-package typescript-mode
   :mode ("\\.ts\\'" . typescript-mode)
   :config
-  (setq typescript-indent-level 2))
+  (setq typescript-indent-level 2)
+  (define-key typescript-mode-map (kbd "C-c C-b") 'run-ts-file)
+  )
 
 (provide 'init-ts)
 (message "init-ts loaded in '%.2f' seconds" (get-time-diff time-marked))
