0ab793b22c08eb703a4b77ba2e03dc672742afae
peter159 <peter.linyi159@gmail.com>
2023-04-09 14:31:04 +0800

    fix run ts file

--- lisp/init-ts.el
+++ lisp/init-ts.el
@@ -28,16 +28,15 @@
 
 (mark-time-here)
 
-(defun run-ts-file ()
+(defun unicorn/run-ts-file ()
   "Compile and run the current TypeScript file."
   (interactive)
   (let* ((file-name (buffer-file-name))
          (base-name (file-name-sans-extension file-name))
          (js-file-name (concat base-name ".js")))
-    (if (not (file-exists-p js-file-name))
-        (progn
-          (message "Compiling %s to %s..." file-name js-file-name)
-          (call-process "tsc" nil nil nil file-name)))
+    (progn
+      (message "Compiling %s to %s..." file-name js-file-name)
+      (call-process "tsc" nil nil nil file-name))
     (message "Running %s..." js-file-name)
     (async-shell-command (concat "node " js-file-name))))
 
@@ -45,7 +44,7 @@
   :mode ("\\.ts\\'" . typescript-mode)
   :config
   (setq typescript-indent-level 2)
-  (define-key typescript-mode-map (kbd "C-c C-b") 'run-ts-file)
+  (define-key typescript-mode-map (kbd "C-c C-b") 'unicorn/run-ts-file)
   )
 
 (provide 'init-ts)
